0e43d728bcf999a825d16c21428bbb0c77b881e1
mgunyho <marci.gunyho@gmail.com>
2020-10-07 20:59:49 +0300

    Add subwindows for header and main content

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,29 @@
 use pancurses::{initscr, endwin, noecho, Input};
 
+const HEADER_SIZE: i32 = 1;
+
 fn main() {
     let window = initscr();
 
-    window.printw("hëllö");
-    window.refresh();
+    let header_win = window.subwin(HEADER_SIZE, 0, 0, 0).unwrap();
+    let main_win = window.subwin(window.get_max_y() - HEADER_SIZE, 0, HEADER_SIZE, 0).unwrap();
+
+    header_win.addstr("this is the header");
+
+    main_win.printw("hëllö");
+
+    header_win.refresh();
+    main_win.refresh();
+
     noecho();
     loop {
         match window.getch() {
             Some(Input::KeyDC) | Some(Input::Character('\x1B')) => break,
-            Some(Input::Character(c)) => { window.addstr(&format!("{}", c)); },
-            Some(input) => { window.addstr(&format!("{:?}", input)); },
+            Some(Input::Character(c)) => { main_win.addstr(&format!("{}", c)); },
+            Some(input) => { main_win.addstr(&format!("{:?}", input)); },
             None => (),
         }
+        main_win.refresh();
     }
 
     endwin();
