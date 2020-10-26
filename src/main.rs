b9e7a2d2cf6e4debe90c0dada668a239f2c08c3a
mgunyho <marci.gunyho@gmail.com>
2020-10-26 14:02:42 +0200

    Only advance search on alphanumeric characters
    This doesn't work with e.g. emojis, but I think that's fine

--- src/main.rs
+++ src/main.rs
@@ -405,7 +405,9 @@ impl TereTui {
                 },
                 Some(Input::KeyDC) => break,
                 Some(Input::Character(c)) => {
-                    self.on_search_char(c);
+                    if c.is_alphanumeric() {
+                        self.on_search_char(c);
+                    }
                 },
                 Some(Input::KeyBackspace) => {
                     self.erase_search_char();
