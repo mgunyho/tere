5fd20eb988f55bac4a55f25d3c5d374ea74d614e
mgunyho <marci.gunyho@gmail.com>
2021-08-10 20:46:12 +0300

    Very quick and dirty implementation of omni-search

--- src/app_state.rs
+++ src/app_state.rs
@@ -6,6 +6,8 @@ use clap::ArgMatches;
 use std::convert::TryFrom;
 use std::ffi::OsStr;
 
+use regex::Regex;
+
 #[path = "settings.rs"]
 mod settings;
 use settings::TereSettings;
@@ -450,13 +452,24 @@ impl TereAppState {
         } else {
             self.search_string.to_lowercase()
         };
+
+        // TODO: make "^" here configurable
+        // TODO: make "omni-search" configurable
+        let mut regex_str = r"^".to_string();
+        regex_str.push_str(&search_string.chars()
+                           .map(|c| c.to_string())
+                           .collect::<Vec<String>>()
+                           // TODO: escape '.' etc in search query...
+                           .join(".*"));
+
+        let search_ptn = Regex::new(&regex_str).unwrap(); //TODO: error handling...
         self.ls_output_buf.apply_filter(|itm| {
             let target = if is_case_sensitive {
                 itm.file_name_checked()
             } else {
                 itm.file_name_checked().to_lowercase()
             };
-            target.starts_with(&search_string)
+            search_ptn.is_match(&target)
         });
     }
 
