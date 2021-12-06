61ef6267baa5ff414473488fd3c648195d76c901
mgunyho <marci.gunyho@gmail.com>
2021-12-06 14:54:13 +0200

    Show parent in debug print

--- src/history.rs
+++ src/history.rs
@@ -104,6 +104,7 @@ impl HistoryTree {
 impl std::fmt::Debug for HistoryTreeEntry {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> Result<(), std::fmt::Error> {
         f.debug_map()
+            .entry(&"parent", &self.parent.upgrade().map(|p| p.label.clone()).unwrap_or("".to_string()))
             .entry(&"label", &self.label)
             .entry(&"last_visited_child", &self.last_visited_child_label().unwrap_or("".to_string()))
             .entry(&"children", &self.children.borrow())
