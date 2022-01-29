088a0872cb39b1ae6e9a481375150366de031291
mgunyho <marci.gunyho@gmail.com>
2022-01-29 10:57:27 +0200

    Use textwrap to wrap help text

--- src/ui/help_window.rs
+++ src/ui/help_window.rs
@@ -1,16 +1,22 @@
 /// Functions for rendering the help window
 
+use std::borrow::Cow;
 use crossterm::style::{ContentStyle, StyledContent};
+use textwrap;
 
 const README_STR: &str = include_str!("../../README.md");
 
-pub fn get_formatted_help_text(w: u16, h: u16) -> StyledContent<String> {
-    let mut res = String::new();
-    //TODO: actual formatting
-    //TODO: check out how tui-rs does line wrapping
-    for line in README_STR.lines().skip(45).take(h as usize) {
-        res.push_str(line);
-        res.push_str("\n");
-    }
-    StyledContent::new(ContentStyle::new(), res)
+pub fn get_formatted_help_text<'a>(w: u16, h: u16) -> Vec<Cow<'a, str>> {
+    let res = &README_STR[
+        README_STR.find("## User guide").expect("Could not find user guide in README")
+            ..
+        README_STR.find("## Similar projects").expect("Could not find end of user guide in README")
+    ];
+
+    //TODO: remove <kbd> etc
+    //TODO: table formatting
+    textwrap::wrap(res, w as usize)
+
+    //TODO: format things with bold etc
+    //StyledContent::new(ContentStyle::new(), res)
 }
