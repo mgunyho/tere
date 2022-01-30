e182f8fe6be0129bad7a3e23c5e5720924db0b59
mgunyho <marci.gunyho@gmail.com>
2022-01-30 12:08:56 +0200

    Return a nested vector of StyledContent

--- src/ui/help_window.rs
+++ src/ui/help_window.rs
@@ -6,8 +6,14 @@ use textwrap;
 
 const README_STR: &str = include_str!("../../README.md");
 
-pub fn get_formatted_help_text<'a>(w: u16, h: u16) -> Vec<Cow<'a, str>> {
-    let res = &README_STR[
+/// Word-wrap the help string to be displayed in the help window, and apply correct formatting
+/// (such as bolding) using crossterm::style.
+///
+/// Returns a vector of vectors, where the outer vector represents lines, and the inner vector
+/// contains either a single string for the whole line, or multiple strings, if the style varies
+/// within the line.
+pub fn get_formatted_help_text<'a>(w: u16, h: u16) -> Vec<Vec<StyledContent<String>>> {
+    let help_str = &README_STR[
         README_STR.find("## User guide").expect("Could not find user guide in README")
             ..
         README_STR.find("## Similar projects").expect("Could not find end of user guide in README")
@@ -15,7 +21,8 @@ pub fn get_formatted_help_text<'a>(w: u16, h: u16) -> Vec<Cow<'a, str>> {
 
     //TODO: remove <kbd> etc
     //TODO: table formatting
-    textwrap::wrap(res, w as usize)
+    let res = textwrap::wrap(help_str, w as usize);
+    res.iter().map(|line| vec![StyledContent::new(ContentStyle::new(), line.to_string())]).collect()
 
     //TODO: format things with bold etc
     //StyledContent::new(ContentStyle::new(), res)
