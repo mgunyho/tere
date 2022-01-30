c3835b585ff70ff6be68b4c25b2af4842614d0ff
mgunyho <marci.gunyho@gmail.com>
2022-01-30 13:39:09 +0200

    Add keyboard shortcuts table justification & formatting

--- src/ui/help_window.rs
+++ src/ui/help_window.rs
@@ -19,12 +19,28 @@ pub fn get_formatted_help_text<'a>(w: u16, h: u16) -> Vec<Vec<StyledContent<Stri
         README_STR.find("## Similar projects").expect("Could not find end of user guide in README")
     ];
 
+    // Skip the table of keyboard shortcuts, we'll format it separately
+    let (help_str, rest) = help_str
+        .split_once("\n\n|")
+        .expect("Could not find keyboard shortcuts table in readme");
+
+    let rest = rest.split_once("\n\n")
+        .expect("Could not find end of keyboard shortcuts table in readme")
+        .1;
+
+    // Add justified keyboard shortcuts table to help string
+    let mut help_str = help_str.to_string();
+    help_str.push_str(&"\n\n"); // add back newlines eaten by split_once
+    help_str.push_str(&get_justified_keyboard_shortcuts_table());
+    help_str.push_str(rest);
+
     // We need to get rid of the `<kbd>` tags before wrapping so it works correctly. We're going to
     // bold all words within backticks, so replace the tags with backticks as well.
     let help_str = help_str
         .replace("<kbd>", "`")
         .replace("</kbd>", "`");
 
+    // apply text wrapping
     let mut help_str = textwrap::wrap(&help_str, w as usize);
 
     let mut res = vec![];
@@ -55,4 +71,59 @@ pub fn get_formatted_help_text<'a>(w: u16, h: u16) -> Vec<Vec<StyledContent<Stri
 
     //res.iter().map(|line| vec![StyledContent::new(ContentStyle::new(), line.to_string())]).collect()
 
+}
+
+
+/// Apply justification to the table of keyboard shortcuts in the README and render it to a String
+/// without the markup
+pub fn get_justified_keyboard_shortcuts_table() -> String {
+    let keyboard_shortcuts = README_STR
+        .split_once("keyboard shortcuts:\n\n")
+        .expect("Couldn't find table of keyboard shortcuts in README")
+        .1;
+    let keyboard_shortcuts = keyboard_shortcuts
+        .split_once("\n\n")
+        .expect("Couldn't find end of keyboard shortcuts table in README")
+        .0;
+
+    let first_column_width = keyboard_shortcuts.lines()
+        .map(|line| line.split("|").skip(1).next().unwrap_or("").len())
+        .max()
+        .unwrap_or(10);
+
+    let mut justified = String::new();
+
+    for (i, line) in keyboard_shortcuts.lines().enumerate() {
+        let cols: Vec<&str> = line.split("|").collect();
+        // cols[0] is empty, because the lines start with '|'.
+        let mut action = cols[1].trim().to_string();
+        let mut shortcut = cols[2].trim().to_string();
+
+        // skip markdown table formatting row
+        if action.starts_with(":--") {
+            continue
+        }
+
+        if i == 0 {
+            // add backticks so that first line is bolded
+            action = format!("`{}`", &action);
+            shortcut = format!("`{}`", &shortcut);
+        }
+
+        justified.push_str(&action);
+
+        // backticks will be removed, so add extra space for them
+        let extra_len = action.chars().filter(|c| *c == '`').count();
+        let padding = first_column_width + extra_len + 2 - action.len();
+        justified.push_str(&" ".repeat(padding));
+        // It's ok to add "\n" at the end of every line, because the split_once() above has
+        // eaten too many newlines from the end anyway.
+        justified.push_str(&shortcut);
+        justified.push('\n');
+    }
+
+    // add extra newline at end
+    justified.push('\n');
+
+    justified
 }
