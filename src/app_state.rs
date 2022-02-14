3cf79b58d1f9ff8441d7604d930ee976c52c821e
mgunyho <marci.gunyho@gmail.com>
2022-02-14 10:33:38 +0200

    Save capture locations in update_matches

--- src/app_state.rs
+++ src/app_state.rs
@@ -64,8 +64,13 @@ impl MatchesVec {
                 } else {
                     item.file_name_checked().to_lowercase()
                 };
-                if search_ptn.is_match(&target) {
-                    Some((i, ())) //TODO: CaptureLocations
+                let mut capture_locations = search_ptn.capture_locations();
+                if let Some(_) = search_ptn.captures_read(&mut capture_locations, &target) {
+                    // have to do it this way using range because capture_locations has no iter() method
+                    let locs = (1..capture_locations.len())
+                        .filter_map(|i| capture_locations.get(i))
+                        .collect();
+                    Some((i, locs))
                 } else {
                     None
                 }
