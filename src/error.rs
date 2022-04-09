c49295b808d09c0edfb4fef30e5d67036c207745
mgunyho <marci.gunyho@gmail.com>
2022-04-09 11:17:38 +0300

    Remove repetitive suffix from error

--- src/error.rs
+++ src/error.rs
@@ -1,20 +1,20 @@
 /// Custom error type
 #[derive(Debug)]
 pub enum TereError {
-    IoError(std::io::Error),
-    ClapError(clap::Error),
-    SerdeJsonError(serde_json::error::Error),
+    Io(std::io::Error),
+    Clap(clap::Error),
+    SerdeJson(serde_json::error::Error),
 }
 
 
 impl From<std::io::Error> for TereError {
-    fn from(e: std::io::Error) -> Self { Self::IoError(e) }
+    fn from(e: std::io::Error) -> Self { Self::Io(e) }
 }
 
 impl From<clap::Error> for TereError {
-    fn from(e: clap::Error) -> Self { Self::ClapError(e) }
+    fn from(e: clap::Error) -> Self { Self::Clap(e) }
 }
 
 impl From<serde_json::error::Error> for TereError {
-    fn from(e: serde_json::error::Error) -> Self { Self::SerdeJsonError(e) }
+    fn from(e: serde_json::error::Error) -> Self { Self::SerdeJson(e) }
 }
