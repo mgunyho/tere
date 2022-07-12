3f13e31ba18b72465e74d0d11e1eba539e23620f
mgunyho <marci.gunyho@gmail.com>
2022-07-12 22:38:17 +0300

    Add script for generating release zip

---
+++ build-release.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# Generate zip file(s) to be uploaded to Github release page
+
+BIN_NAME=tere
+
+rm -rf release
+mkdir release
+
+# from https://github.com/Canop/broot/blob/master/version.sh
+version=$(sed 's/^version = "\([^\"]*\)"/\1/;t;d' Cargo.toml | head -1)
+
+echo "Building $BIN_NAME version $version"
+
+#cargo clean
+#TODO: more targets
+target="x86_64-unknown-linux-gnu"
+echo $target
+cargo build --target=$target --release 
+
+cd target/$target/release
+zip_name=$BIN_NAME-$version-$target.zip
+zip $zip_name $BIN_NAME
+cd -
+mv -v target/$target/release/$zip_name release/
