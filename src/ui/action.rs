7c09c215444470a4cf0349ebf9b87349461ad0df
Marton Gunyho <andras.gunyho@aalto.fi>
2022-08-23 09:31:55 +0300

    Add enum for possible actions

---
+++ src/ui/action.rs
@@ -0,0 +1,43 @@
+/// The possible actions that the user can do
+
+
+pub enum Action {
+    /// Change to the directory under the cursor
+    ChangeDir,
+    /// Change to the parent directory
+    ChangeDirParent,
+    ChangeDirHome,
+    ChangeDirRoot,
+    /// Change to the directory under the cursor and exit
+    ChangeDirAndExit,
+
+    CursorUp,
+    CursorDown,
+    /// Move cursor down by one page
+    CursorUpPage,
+    /// Move cursor up by one page
+    CursorDownPage,
+    /// Move cursor to first item
+    CursorFirst,
+    /// Move cursor to last item
+    CursorLast,
+
+    /// Clear the current search
+    ClearSearch,
+    /// Clear the current search, or if it's empty, exit
+    ClearSearchOrExit,
+
+    /// Cycle the case sensitivity mode
+    ChangeCaseSensitiveMode,
+    /// Cycle the gap search mode
+    ChangeGapSearchMode,
+
+    RefreshListing,
+
+    /// Show the help screen
+    Help,
+
+    Exit,
+    /// Exit without changing the directory
+    ExitWithoutCd,
+}
