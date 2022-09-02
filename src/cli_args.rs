d79a6fe7b1aaffe6c10b94e33323c155ccd58d99
mgunyho <marci.gunyho@gmail.com>
2022-09-02 10:42:56 +0300

    Use updated macros, now static lifetime works

--- src/cli_args.rs
+++ src/cli_args.rs
@@ -64,46 +64,66 @@ pub fn get_cli_args() -> App<'static> {
              .long("case-sensitive")
              .short('s')  // same as ripgrep
              .help("Case sensitive search")
-             .long_help(format!("Enable case-sensitive search.\n\n{}",
-                                case_sensitive_template!("ignore-case", "smart-case")).as_str())
+             .long_help(case_sensitive_template!(
+                     "Enable case-sensitive search.",
+                     "ignore-case",
+                     "smart-case"
+            ))
              .overrides_with_all(&["ignore-case", "smart-case", "case-sensitive"])
             )
         .arg(Arg::new("ignore-case")
              .long("ignore-case")
              .short('i') // same as ripgrep
              .help("Ignore case when searching")
-             .long_help(format!("Enable case-insensitive search.\n\n{}",
-                                case_sensitive_template!("case-sensitive", "smart-case")).as_str())
+             .long_help(case_sensitive_template!(
+                     "Enable case-insensitive search.",
+                     "case-sensitive",
+                     "smart-case"
+                     ))
              .overrides_with_all(&["smart-case", "ignore-case"])
             )
         .arg(Arg::new("smart-case")
              .long("smart-case")
              .short('S') // same as ripgrep
              .help("Smart case search (default)")
-             .long_help(format!("Enable smart-case search. If the search query contains only lowercase letters, search case insensitively. Otherwise search case sensitively. This is the default search mode.\n\n{}",
-                                case_sensitive_template!("case-sensitive", "ignore-case")).as_str())
+             .long_help(case_sensitive_template!(
+                     "Enable smart-case search. If the search query contains only lowercase letters, search case insensitively. Otherwise search case sensitively. This is the default search mode.",
+                     "case-sensitive",
+                     "ignore-case"
+                     ))
              .overrides_with("smart-case")
             )
         .arg(Arg::new("gap-search")
              .long("gap-search")
              .short('g')
              .help("Match the search from the beginning, but allow gaps (default)")
-             .long_help(format!("When searching, match items that start with the same character as the search query, but allow gaps between the search characters. For example, searching for \"do\" would match \"DesktOp\", \"DOcuments\", and \"DOwnloads\", while searching for \"dt\" would match \"DeskTop\" and \"DocumenTs\" but not \"downloads\", and searching for \"es\" would match none of the above. This is the default behavior.\n\n{}", gap_search_mode_template!("gap-search", "no-gap-search")).as_str())
+             .long_help(gap_search_mode_template!(
+                     "When searching, match items that start with the same character as the search query, but allow gaps between the search characters. For example, searching for \"do\" would match \"DesktOp\", \"DOcuments\", and \"DOwnloads\", while searching for \"dt\" would match \"DeskTop\" and \"DocumenTs\" but not \"downloads\", and searching for \"es\" would match none of the above. This is the default behavior.",
+                     "gap-search",
+                     "no-gap-search"
+                     ))
              .overrides_with_all(&["gap-search", "gap-search-anywhere", "no-gap-search"])
             )
         .arg(Arg::new("gap-search-anywhere")
              .long("gap-search-anywhere")
              .short('G')
              .help("Match the search anywhere, and allow gaps")
-             .long_help(format!("When searching, allow the search characters to appear anywhere in a file/folder name, possibly with gaps between them. For example, searching for \"do\" would match \"DesktOp\", \"DOcuments\", and \"DOwnloads\", while searching for \"es\" would match \"dESktop\" and \"documEntS\", but not \"downloads\".\n\n{}",
-                                gap_search_mode_template!("gap-search-from-start", "no-gap-search")).as_str())
+             .long_help(gap_search_mode_template!(
+                     "When searching, allow the search characters to appear anywhere in a file/folder name, possibly with gaps between them. For example, searching for \"do\" would match \"DesktOp\", \"DOcuments\", and \"DOwnloads\", while searching for \"es\" would match \"dESktop\" and \"documEntS\", but not \"downloads\".",
+                     "gap-search-from-start",
+                     "no-gap-search"
+                     ))
              .overrides_with_all(&["gap-search-anywhere", "no-gap-search"])
             )
         .arg(Arg::new("no-gap-search")
              .long("no-gap-search")
              .short('n')
              .help("Match the search from the beginning, and do not allow gaps")
-             .long_help(format!("Disable gap-search. Match only consecutive characters from the beginning of the search query. For example, searching for \"do\" would match \"DOcuments\" and \"DOwnloads\", but not \"desktop\".\n\n{}", gap_search_mode_template!("gap-search", "gap-search-from-start")).as_str())
+             .long_help(gap_search_mode_template!(
+                     "Disable gap-search. Match only consecutive characters from the beginning of the search query. For example, searching for \"do\" would match \"DOcuments\" and \"DOwnloads\", but not \"desktop\".",
+                     "gap-search",
+                     "gap-search-from-start"
+                     ))
              .overrides_with("no-gap-search")
             )
         //TODO: if somebody wants this: '-N', '--no-gap-search-anywhere - don't allow gaps, but can start anywhere. maybe have to come up with a better long name.
