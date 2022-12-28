dfc22129506c1ea69e9a1b4080ad9d3916f48cc6
mgunyho <marci.gunyho@gmail.com>
2022-12-28 10:51:01 +0200

    Implement write_version_file and use it

--- src/installation_check.rs
+++ src/installation_check.rs
@@ -11,18 +11,21 @@ use crate::error::TereError;
 /// the prompt, write the `version` file and return Ok, otherwise return an error.
 pub fn check_first_run_with_prompt(settings: &TereSettings) -> Result<(), TereError> {
     let hist_file = &settings.history_file;
-    let version_file = version_file_path();
+    let version_file_path = version_file_path();
 
     // Check if the version file exists to determine whether we want to show the first run prompt.
     // Additionally, to be backwards compatible and not show the prompt to old users, use a bit of a
     // heuristic: we assume the app has been run before if the history file exists, or the user
     // explicitly requests no history file.
-    if version_file.is_none() // chache dir doesn't exist, we assume that the user knows what they're doing
-        || version_file.unwrap().try_exists().unwrap_or(false) // version file exists
+    if version_file_path.is_none() // chache dir doesn't exist, we assume that the user knows what they're doing
+        || version_file_path.as_ref().unwrap().try_exists().unwrap_or(false) // version file exists
         || hist_file.is_none() // user passed empty history file
         || PathBuf::from(hist_file.as_ref().unwrap()).try_exists().unwrap_or(false) // history file exists
     {
-        //TODO: write version file
+        // Only attempt to write the version file if the cache directory exists
+        if version_file_path.is_some() {
+            write_version_file()?;
+        }
         Ok(())
     } else {
         prompt_first_run()
@@ -39,5 +42,5 @@ fn version_file_path() -> Option<PathBuf> {
 }
 
 fn write_version_file() -> Result<(), TereError> {
-    todo!()
+    std::fs::write(version_file_path().unwrap(), env!("CARGO_PKG_VERSION")).map_err(TereError::from)
 }
