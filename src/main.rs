74a6252f49527a34fce82196f7fa3a33cdd502b7
mgunyho <marci.gunyho@gmail.com>
2022-12-31 17:42:52 +0200

    Fix indentation

--- src/main.rs
+++ src/main.rs
@@ -44,7 +44,7 @@ fn main() -> Result<(), TereError> {
     //TODO: should this alternate screen etc initialization (and teardown) be done by the UI?
     //Now the mouse capture enabling (which is kind of similar) is handled there.
     execute!(std::io::stderr(), terminal::EnterAlternateScreen)?;
-    let res = {
+    let res: Result<std::path::PathBuf, TereError> = {
         // We can use unwrap here, the guards will ensure that the panic is handled correctly.
         let _guard = GuardWithHook::new(|| execute!(std::io::stderr(), terminal::LeaveAlternateScreen).unwrap());
         execute!(std::io::stderr(), cursor::Hide).unwrap();
@@ -54,19 +54,17 @@ fn main() -> Result<(), TereError> {
             {
                 let _guard = GuardWithHook::new(|| terminal::disable_raw_mode().unwrap());
 
-    // we are now inside the alternate screen, so collect all errors and attempt
-    // to leave the alt screen in case of an error
+                // we are now inside the alternate screen, so collect all errors and attempt
+                // to leave the alt screen in case of an error
 
-    let mut stderr = std::io::stderr();
+                let mut stderr = std::io::stderr();
 
-    let res: Result<std::path::PathBuf, TereError> = stderr.flush().map_err(TereError::from)
-        .and_then(|_| TereSettings::parse_cli_args(&cli_args))
-        .and_then(|(settings, warnings)| { check_first_run_with_prompt(&settings, &mut stderr)?; Ok((settings, warnings)) })
-        .and_then(|(settings, warnings)| TereAppState::init(settings, &warnings))
-        .and_then(|state| TereTui::init(state, &mut stderr))
-        .and_then(|mut ui| ui.main_event_loop()); // actually run the app and return the final path
-
-    res
+                stderr.flush().map_err(TereError::from)
+                    .and_then(|_| TereSettings::parse_cli_args(&cli_args))
+                    .and_then(|(settings, warnings)| { check_first_run_with_prompt(&settings, &mut stderr)?; Ok((settings, warnings)) })
+                    .and_then(|(settings, warnings)| TereAppState::init(settings, &warnings))
+                    .and_then(|state| TereTui::init(state, &mut stderr))
+                    .and_then(|mut ui| ui.main_event_loop()) // actually run the app and return the final path
             }
         }
     };
