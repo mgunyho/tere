f8f732c75de6890e2edca5758b053214f05760f7
mgunyho <20118130+mgunyho@users.noreply.github.com>
2023-08-24 18:18:02 +0300

    Add test for first run prompt
    Note: there's no way to mark the test as 'ignored' for now, so the test just passes if you already have a history file. Could maybe be fixed with an env variable...

--- tests/cli.rs
+++ tests/cli.rs
@@ -1,8 +1,10 @@
 //use regex::bytes::Regex;
 use regex::Regex;
 use rexpect::error::Error as RexpectError;
-use rexpect::session::{spawn as rexpect_spawn, PtySession};
+use rexpect::session::{spawn_command, PtySession};
 use std::io::Write;
+use std::process::Command;
+use tempfile::tempdir;
 
 /// Strip a string until the 'alternate screen exit' escape code, and return the slice containing
 /// the remaining string.
@@ -16,19 +18,30 @@ fn strip_until_alternate_screen_exit(text: &str) -> &str {
     }
 }
 
-/// Initialize the app and wait until it has entered the alternate screen, and return a handle to
-/// the rexpect PtySession, which is ready for input.
+/// Initialize the app and wait until it has entered the alternate screen. Returns a handle to the
+/// rexpect PtySession, which is ready for input. Panics if initializing the app fails.
 fn run_app() -> PtySession {
-    let mut proc = rexpect_spawn(
-        // explicitly pass empty history file so we don't get first run prompt
-        &format!("{} --history-file ''", env!("CARGO_BIN_EXE_tere")),
-        Some(1_000),
-    )
-    .expect("error spawning process");
+    let mut cmd = get_cmd();
+    // explicitly pass empty history file so we don't get first run prompt
+    // NOTE: cannot directly chain this with get_cmd(), otherwise we get a mutable ref which we
+    // can't move into run_app_with_cmd.
+    cmd.args(["--history-file", ""]);
+    run_app_with_cmd(cmd)
+}
+
+/// Initialize app with the given Command object and wait until it has entered the alternate screen.
+/// Returns a handle to the rexpect PtySession, which is ready for input. Panics if initializing
+/// the app fails.
+fn run_app_with_cmd(cmd: Command) -> PtySession {
+    let mut proc = spawn_command(cmd, Some(1_000)).expect("error spawning process");
 
     // \u{1b}[?1049h - enter alternate screen
     proc.exp_string("\x1b[?1049h").unwrap();
     proc
+}
+
+fn get_cmd() -> Command {
+    Command::new(env!("CARGO_BIN_EXE_tere"))
 }
 
 #[test]
@@ -42,6 +55,29 @@ fn output_on_exit_without_cd() -> Result<(), RexpectError> {
     let output = strip_until_alternate_screen_exit(&output);
 
     assert_eq!(output, "tere: Exited without changing folder\r\n");
+
+    Ok(())
+}
+
+#[test]
+fn first_run_prompt_if_history_file_doesnt_exist() -> Result<(), RexpectError> {
+    let mut cmd = get_cmd();
+
+    // set the XDG_CACHE_HOME to point to a temporary folder so that we always get the first run prompt
+    let tmp = tempdir().expect("error creating temporary folder");
+    cmd.env("XDG_CACHE_HOME", tmp.path().as_os_str());
+
+    let mut proc = run_app_with_cmd(cmd);
+    proc.send("n")?;
+    proc.writer.flush()?;
+    let output = proc.exp_eof()?;
+
+    let ptn = Regex::new("It seems like you are running.*for the first time").unwrap();
+    // check that first run prompt message is there
+    assert!(ptn.find(&output).is_some());
+
+    // check that having pressed 'n' prints the expected message
+    assert_eq!(strip_until_alternate_screen_exit(&output), "Cancelled.");
 
     Ok(())
 }
