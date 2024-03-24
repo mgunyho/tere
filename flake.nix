077911b648b66e2119e860dfb6121f7839d36fbc
Producer Matt <58014742+ProducerMatt@users.noreply.github.com>
2024-03-24 12:02:51 -0500

    nix: use rustPlatform build from Nixpkgs

--- flake.nix
+++ flake.nix
@@ -1,17 +1,36 @@
 {
   inputs = {
-    naersk.url = "github:nix-community/naersk/master";
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, utils, naersk }:
+  outputs = { self, nixpkgs, utils }:
     utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
-        naersk-lib = pkgs.callPackage naersk { };
+        tere = with pkgs; rustPlatform.buildRustPackage {
+          pname = "tere";
+          version = "1.5.1";
+
+          src = ./.;
+
+          cargoHash = "sha256-EnAO82q8ErchCDjn1bXNxHyx+sXRJB1NpSATpOtTs9s=";
+
+          postPatch = ''
+          rm .cargo/config.toml;
+          '';
+
+          meta = with lib; {
+            description = "A faster alternative to cd + ls";
+            homepage = "https://github.com/mgunyho/tere";
+            license = licenses.eupl12;
+            maintainers = with maintainers; [ ProducerMatt ];
+            mainProgram = "tere";
+          };
+        };
+
       in {
-        defaultPackage = naersk-lib.buildPackage ./.;
+        defaultPackage = tere;
         devShell = with pkgs;
           mkShell {
             buildInputs = [
