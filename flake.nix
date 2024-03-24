525b977bd0132530ffe8220c807064968e4eb490
Producer Matt <58014742+ProducerMatt@users.noreply.github.com>
2024-03-24 11:26:56 -0500

    nix: format, add nil_ls and nixfmt

--- flake.nix
+++ flake.nix
@@ -10,12 +10,23 @@
       let
         pkgs = import nixpkgs { inherit system; };
         naersk-lib = pkgs.callPackage naersk { };
-      in
-      {
+      in {
         defaultPackage = naersk-lib.buildPackage ./.;
-        devShell = with pkgs; mkShell {
-          buildInputs = [ cargo rustc rustfmt pre-commit rustPackages.clippy ];
-          RUST_SRC_PATH = rustPlatform.rustLibSrc;
-        };
+        devShell = with pkgs;
+          mkShell {
+            buildInputs = [
+              # rust packages
+              cargo
+              rustc
+              rustfmt
+              pre-commit
+              rustPackages.clippy
+
+              # Nix conveniences
+              nil
+              nixpkgs-fmt
+            ];
+            RUST_SRC_PATH = rustPlatform.rustLibSrc;
+          };
       });
 }
