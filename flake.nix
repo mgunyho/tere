c8faba8b7d2a7ed849343ca2c048e8ae06f3fdeb
Producer Matt <58014742+ProducerMatt@users.noreply.github.com>
2024-03-30 15:38:29 -0500

    use buildRustCrate, allow Musl building

--- flake.nix
+++ flake.nix
@@ -5,51 +5,38 @@
       url = "github:hercules-ci/flake-parts";
       inputs.nixpkgs-lib.follows = "nixpkgs";
     };
-    #crate2nix.url = "github:nix-community/crate2nix";
+    crate2nix.url = "github:nix-community/crate2nix?tag=crate2nix-v0.12.0";
   };
 
-  outputs = inputs@{ self, nixpkgs, flake-parts, ... }:
+  outputs = inputs@{ self, nixpkgs, flake-parts, crate2nix, ... }:
   flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [
         "x86_64-linux"
         "aarch64-linux"
-        "x86_64-linux"
+        "x86_64-darwin"
         "aarch64-darwin"
       ];
 
       perSystem = { system, pkgs, lib, inputs', ... }:
       let
-        pkgs = import nixpkgs { inherit system; };
-        tere = with pkgs; rustPlatform.buildRustPackage {
-          pname = "tere";
-          version = "1.5.1";
-
-          src = ./.;
-
-          # this fails with git cargo dependencies, if needed switch to cargoHash
-          cargoLock = {
-            lockFile = ./Cargo.lock;
-          };
-
-          preBuild = ''
-          export RUST_BACKTRACE=1
-          '';
-
-          postPatch = ''
-          rm .cargo/config.toml;
-          '';
-
-          meta = with lib; {
-            description = "A faster alternative to cd + ls";
-            homepage = "https://github.com/mgunyho/tere";
-            license = licenses.eupl12;
-            maintainers = with maintainers; [ ProducerMatt ];
-            mainProgram = "tere";
-          };
-        };
-
+        mkTere = {pkgs ? pkgs}:
+          ((inputs.crate2nix.tools.${system}.appliedCargoNix {
+            name = "tere";
+            src = ./.;
+          }).rootCrate.build // {
+            runTests = true;
+            meta = with lib; {
+              description = "A faster alternative to cd + ls";
+              homepage = "https://github.com/mgunyho/tere";
+              license = licenses.eupl12;
+              maintainers = with maintainers; [ ProducerMatt ];
+              mainProgram = "tere";
+            };});
       in {
-        packages.default = tere;
+        checks.default = mkTere {};
+        packages.default = mkTere {};
+        checks.musl = mkTere {pkgs = pkgs.pkgsMusl;};
+        packages.musl = mkTere {pkgs = pkgs.pkgsMusl;};
         devShells.default = with pkgs;
           mkShell {
             buildInputs = [
@@ -62,8 +49,11 @@
               rustPackages.clippy
 
               # Nix conveniences
+              nixUnstable
               nil
-              nixpkgs-fmt
+              alejandra
+              statix
+              deadnix
             ];
             RUST_SRC_PATH = rustPlatform.rustLibSrc;
           };
