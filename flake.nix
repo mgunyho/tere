1db8a0ee80f297c522dc4ce859b7df2387415b68
Producer Matt <58014742+ProducerMatt@users.noreply.github.com>
2024-03-31 16:14:35 -0500

    try Crane build system

--- flake.nix
+++ flake.nix
@@ -1,60 +1,140 @@
 {
+  description = "Build a cargo project";
+
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    flake-parts = {
-      url = "github:hercules-ci/flake-parts";
-      inputs.nixpkgs-lib.follows = "nixpkgs";
+
+    crane = {
+      url = "github:ipetkov/crane";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
-    crate2nix.url = "github:nix-community/crate2nix?tag=crate2nix-v0.12.0";
+
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.rust-analyzer-src.follows = "";
+    };
+
+    flake-utils.url = "github:numtide/flake-utils";
+
+    advisory-db = {
+      url = "github:rustsec/advisory-db";
+      flake = false;
+    };
   };
 
-  outputs = inputs@{ self, nixpkgs, flake-parts, crate2nix, ... }:
-  flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [
-        "x86_64-linux"
-        "aarch64-linux"
-        "x86_64-darwin"
-        "aarch64-darwin"
-      ];
-
-      perSystem = { system, pkgs, lib, inputs', ... }:
+  outputs = { nixpkgs, crane, fenix, flake-utils, advisory-db, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
       let
-        mkTere = {pkgs ? pkgs}:
-          ((inputs.crate2nix.tools.${system}.appliedCargoNix {
-            name = "tere";
-            src = ./.;
-          }).rootCrate.build.override {
-              runTests = true;
-            } // {
-            meta = with lib; {
-              description = "A faster alternative to cd + ls";
-              homepage = "https://github.com/mgunyho/tere";
-              license = licenses.eupl12;
-              maintainers = with maintainers; [ ProducerMatt ];
-              mainProgram = "tere";
-            };});
+        pkgs = nixpkgs.legacyPackages.${system};
 
-          ## run the tests via the script command so that the integration tests have a TTY
-          #checkPhase = ''
-          #script -c 'cargo test'
-          #'';
+        inherit (pkgs) lib;
 
-          #buildInputs = [
-          #  util-linux  # 'script' command
-          #];
-      in {
-        checks.default = mkTere {};
-        packages.default = mkTere {};
-        checks.musl = mkTere {pkgs = pkgs.pkgsMusl;};
-        packages.musl = mkTere {pkgs = pkgs.pkgsMusl;};
-        devShells.default = with pkgs;
-          mkShell {
-            buildInputs = [
+        craneLib = crane.lib.${system};
+        src = (craneLib.path ./.);
+
+        # Common arguments can be set here to avoid repeating them later
+        commonArgs = {
+          inherit src;
+          strictDeps = true;
+
+          buildInputs = [
+            # Add additional build inputs here
+          ] ++ lib.optionals pkgs.stdenv.isDarwin [
+            # Additional darwin specific inputs can be set here
+            pkgs.libiconv
+          ];
+
+          preBuild = ''
+          export RUST_BACKTRACE=1
+          '';
+
+          postPatch = ''
+          rm .cargo/config.toml;
+          '';
+
+          checkPhase = ''
+            ${pkgs.unixtools.script}/bin/script -qfec 'cargo test' /dev/null
+          '';
+
+          meta = with lib; {
+            description = "A faster alternative to cd + ls";
+            homepage = "https://github.com/mgunyho/tere";
+            license = licenses.eupl12;
+            maintainers = with maintainers; [ ProducerMatt ];
+            mainProgram = "tere";
+          };
+        };
+
+        craneLibLLvmTools = craneLib.overrideToolchain
+          (fenix.packages.${system}.complete.withComponents [
+            "cargo"
+            "llvm-tools"
+            "rustc"
+          ]);
+
+        # Build *just* the cargo dependencies, so we can reuse
+        # all of that work (e.g. via cachix) when running in CI
+        cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+
+        # Build the actual crate itself, reusing the dependency
+        # artifacts from above.
+        my-crate = craneLib.buildPackage (commonArgs // {
+          inherit cargoArtifacts;
+        });
+      in
+      {
+        checks = {
+          # Build the crate as part of `nix flake check` for convenience
+          inherit my-crate;
+
+          # Run clippy (and deny all warnings) on the crate source,
+          # again, reusing the dependency artifacts from above.
+          #
+          # Note that this is done as a separate derivation so that
+          # we can block the CI if there are issues here, but not
+          # prevent downstream consumers from building our crate by itself.
+          my-crate-clippy = craneLib.cargoClippy (commonArgs // {
+            inherit cargoArtifacts;
+            cargoClippyExtraArgs = "--all-targets -- --deny warnings";
+          });
+
+          my-crate-doc = craneLib.cargoDoc (commonArgs // {
+            inherit cargoArtifacts;
+          });
+
+          # Check formatting
+          my-crate-fmt = craneLib.cargoFmt {
+            inherit src;
+          };
+
+          # Audit dependencies
+          my-crate-audit = craneLib.cargoAudit {
+            inherit src advisory-db;
+          };
+
+          # Audit licenses
+          my-crate-deny = craneLib.cargoDeny {
+            inherit src;
+          };
+        };
+        packages = {
+          default = my-crate;
+        } // lib.optionalAttrs (!pkgs.stdenv.isDarwin) {
+          my-crate-llvm-coverage = craneLibLLvmTools.cargoLlvmCov (commonArgs // {
+            inherit cargoArtifacts;
+          });
+        };
+
+        apps.default = flake-utils.lib.mkApp {
+          drv = my-crate;
+        };
+
+        devShells.default = with pkgs; craneLib.devShell {
+          # Extra inputs can be added here; cargo and rustc are provided by default.
+          packages = [
               # rust packages
-              cargo
-              rustc
               rustfmt
-              rust-analyzer
               pre-commit
               rustPackages.clippy
 
@@ -66,6 +146,6 @@
               deadnix
             ];
             RUST_SRC_PATH = rustPlatform.rustLibSrc;
-          };
-      };};
+        };
+      });
 }
